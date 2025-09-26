a20651c013f5090da6f1541ff2e219bf65ede4b6
JonahThurston <jonah.m.thurston@gmail.com>
2025-09-25 18:33:25 -0600

    Create setup.sql

--- aisql/setup.sql
+++ aisql/setup.sql
@@ -1,48 +1,28 @@
-create table person (
-    person_id integer primary key,
-    name varchar(20) not null
+CREATE TABLE Player (
+  player_id INTEGER PRIMARY KEY,
+  faction   VARCHAR(20) NOT NULL,
+  points    INTEGER NOT NULL
 );
 
-create table phone (
-    phone_id integer primary key,
-    person_id integer not null,
-    area_code int not null,
-    number int not null,
-    can_recieve_sms tinyint not null,
-    foreign key (person_id) references person (person_id)
+CREATE TABLE Clearing (
+  clearing_id INTEGER PRIMARY KEY,
+  suite       VARCHAR(20) NOT NULL
 );
 
-create table address (
-    address_id integer primary key,
-    person_id integer not null,
-    street varchar(50),
-    zip integer not null
+CREATE TABLE Piece (
+  piece_id    INTEGER PRIMARY KEY,
+  clearing_id INTEGER NOT NULL,
+  player_id   INTEGER NOT NULL,
+  piece_type  VARCHAR(20)   NOT NULL,
+  FOREIGN KEY (player_id)   REFERENCES Player(player_id),
+  FOREIGN KEY (clearing_id) REFERENCES Clearing(clearing_id)
 );
 
-create table zip (
-    zip integer primary key,
-    city varchar(35),
-    state_two_letter_code char(2)
-);
-
-create table dog (
-    dog_id integer primary key,
-    name varchar(35),
-    breed varchar(35),
-    birth_date date
-);
-
-create table award (
-    award_id integer primary key,
-    dog_id integer not null,
-    event_date date,
-    award_name varchar(25) not null,
-    foreign key (dog_id) references dog (dog_id)
-);
-
-create table person_dog (
-    dog_id integer,
-    person_id integer,
-    foreign key (dog_id) references dog (dog_id),
-    foreign key (person_id) references person (person_id)
+CREATE TABLE Cards (
+  card_id   INTEGER PRIMARY KEY,
+  title     TEXT    NOT NULL,
+  effect    TEXT    NOT NULL,
+  cost      INTEGER NOT NULL,
+  player_id INTEGER,
+  FOREIGN KEY (player_id) REFERENCES Player(player_id)
 );
